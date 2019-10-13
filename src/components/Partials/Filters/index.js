60f3ae48b1841e3563b70a00254f432ca89da0b7
piotrslowik <33666830+piotrslowik@users.noreply.github.com>
2019-10-13 17:33:02 +0200

    Minor filters styling fixes

--- src/components/Partials/Filters/index.js
+++ src/components/Partials/Filters/index.js
@@ -8,11 +8,14 @@ import data from '../../../data.json'
 class Filters extends Component {
   
     render() {
+        const currentYear = new Date().getFullYear();
         return (
             <div className="Filters">
                 <LabelledSelect label="Paliwo" values={['Dowolne', ...data.fuelTypes]} />
                 <LabelledSelect label="Pochodzenie" values={['Dowolne', ...data.origin.sort(), "Inne"]} />
                 <Range label="Przebieg"></Range>
+                {/* <Range label="Cena"></Range> */}
+                <Range label="Rocznik" min={currentYear - 100} max={currentYear}></Range>
             </div>
         )
     }
